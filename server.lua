b541dd397770c49b3a20e8b4ed22fe70e1a63328
Atomik <138856192+atomikk0@users.noreply.github.com>
2024-09-14 13:10:55 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,8 +2,8 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('atomik-stash:server:depo', function(id, label, slots, weight, job)
     if job then
-        exports.ox_inventory:RegisterStash(id, label, slots, weight, true, job)
+        exports.ox_inventory:RegisterStash(id, label, slots, weight, false, job)
     else
-        exports.ox_inventory:RegisterStash(id, label, slots, weight, true)
+        exports.ox_inventory:RegisterStash(id, label, slots, weight, false)
     end
 end)
